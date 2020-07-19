c8d603d58036b1b5deb6894bbb4f3d90d1a664d0
jokw1228 <jokw1228@naver.com>
2020-07-19 23:43:02 +0900

    버튼 클릭 시 신 전환
    Button.cs 에 코드가 추가되었습니다.
    문자열 go_to_scene에 도달할 신의 이름을 적어 놓으면 됩니다.

--- Assets/Script/Button.cs
+++ Assets/Script/Button.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
 {
+    public string go_to_scene;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,7 @@ public class Button : MonoBehaviour
 
     void OnMouseDown()
     {
-        Debug.Log("Button has been clicked(OnMouseDown).");
+        Debug.Log("Scene has been changed to " + go_to_scene);
+        SceneManager.LoadScene(go_to_scene);
     }
 }
