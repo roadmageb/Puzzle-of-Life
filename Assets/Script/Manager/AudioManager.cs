7a89feeae311d047e3d2a72f5d1e05929e27e611
jokw1228 <jokw1228@naver.com>
2020-08-24 01:15:35 +0900

    오디오 매니저 추가
    퍼즐 클리어 효과음, 퍼즐 초기화 효과음, 세포 세대 변화 효과음(GenerationReplacement) 추가

---
+++ Assets/Script/Manager/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    public AudioSource sndPuzzleClear;
+    public AudioSource sndPuzzleReset;
+    public AudioSource sndGenerationReplacement;
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+    public void PuzzleClear()
+    {
+        sndPuzzleClear.Play();
+    }
+    public void PuzzleReset()
+    {
+        sndPuzzleReset.Play();
+    }
+    public void GenerationReplacement()
+    {
+        sndGenerationReplacement.Play();
+    }
+}
